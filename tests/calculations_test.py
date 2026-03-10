9cfc03b6c22c5fa553ecd9f08783ee5f3bcd3fca
tristinEcomplete <tristin@ecomplete.co.za>
2026-03-10 14:37:46 +0000

    Test Changes

--- tests/calculations_test.py
+++ tests/calculations_test.py
@@ -53,6 +53,16 @@ def test_get_nth_fibonacci_one():
 
     # Act
     result = get_nth_fibonacci(n)
+def test_get_nth_fibonacci_ten():
+ """Test with n=10."""
+ # Arrange
+ n = 10
+
+ # Act
+ result = get_nth_fibonacci(n)
+
+ # Assert
+ assert result == 89
 
     # Assert
     assert result == 1
