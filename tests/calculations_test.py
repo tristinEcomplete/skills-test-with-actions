db27f83e6a3945be99785943523fcf6b33f9835d
tristinEcomplete <tristin@ecomplete.co.za>
2026-03-11 12:20:06 +0000

    New Commit

--- tests/calculations_test.py
+++ tests/calculations_test.py
@@ -63,18 +63,3 @@ def test_get_nth_fibonacci_ten():
 
  # Assert
  assert result == 89
-
-    # Assert
-assert result == 1
-
-
-# def test_get_nth_fibonacci_ten():
-#     """Test with n=10."""
-#     # Arrange
-#     n = 10
-
-#     # Act
-#     result = get_nth_fibonacci(n)
-
-#     # Assert
-#     assert result == 89
